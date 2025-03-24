51295fcae7859327d1bd0aea3817b71d5fa4fe74
jiyun7765 <jiyun2422@gmail.com>
2025-03-24 14:54:43 +0900

    v0.7 array

--- week03.py
+++ week03.py
@@ -1,14 +1,6 @@
-def move_zeros(l):
-    zero_idx = 0
-    for i in range(len(l)):
-        n = l[i]
-        if n != 0:
-            l[zero_idx] = n
-            if zero_idx != i:
-                l[i] = 0
-            zero_idx = zero_idx + 1
-    return l
+groups = ['HOT', 'Seventeen', 'Black Pink', 'NJZ']
+# ratings = [1, ,2 ,4, 3, 100]
+ratings = [1, 2, 4, 3]
 
-l = [99, 0, -7, 0, 16]
-move_zeros(l)
-print(l)
+groups_rating = list(zip(groups, ratings))
+print(groups_rating)
