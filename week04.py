39c986221699c8050af0967ee3bda4e8aae200ff
jiyun7765 <jiyun2422@gmail.com>
2025-03-31 15:08:06 +0900

    .\.git\

--- week04.py
+++ week04.py
@@ -1,3 +1,5 @@
+import random
+
 class Node:
     def __init__(self, data, link=None):
         self.data = data
@@ -38,10 +40,16 @@ class LinkedList:
         return out_texts + "END"
 
 
+# ll = LinkedList()
+# ll.append(8)
+# ll.append(10)
+# ll.append(-9)
+# print(ll)
+# print(ll.search(99))
+# print(ll.search(10))
+
 ll = LinkedList()
-ll.append(8)
-ll.append(10)
-ll.append(-9)
+for i in range(20):
+    ll.append(random.randint(1,30))
 print(ll)
-print(ll.search(99))
-print(ll.search(10))
+print(ll.search(7))
