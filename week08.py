a3e8edb0a1c8f4495889ec106f9cef806236240a
jiyun7765 <jiyun2422@gmail.com>
2025-04-28 15:14:18 +0900

    v3.3 tree

--- week08.py
+++ week08.py
@@ -1,27 +1,3 @@
-def pre_order(node):
-    if node is None:
-        return
-    print(node.data, end='-')
-    pre_order(node.left)
-    pre_order(node.right)
-
-
-def in_order(node):
-    if node is None:
-        return
-    in_order(node.left)
-    print(node.data, end='-')
-    in_order(node.right)
-
-
-def post_order(node):
-    if node is None:
-        return
-    post_order(node.left)
-    post_order(node.right)
-    print(node.data, end='-')
-
-
 class TreeNode:
     def __init__(self):
         self.left = None
@@ -29,32 +5,38 @@ class TreeNode:
         self.right = None
 
 
-node1 = TreeNode()
-node1.data = 'hs'
-
-node2 = TreeNode()
-node2.data = 'sl'
-node1.left = node2
-
-node3 = TreeNode()
-node3.data = 'mb'
-node1.right = node3
-
-node4 = TreeNode()
-node4.data = 'hw'
-node2.left = node4
-
-node5 = TreeNode()
-node5.data = 'zz'
-node2.right = node5
-
-node6 = TreeNode()
-node6.data = 'sm'
-node3.left = node6
+def post_order(node):
+    if node:
+        post_order(node.left)
+        post_order(node.right)
+        print(node.data, end='->')
 
 
-post_order(node1)
-print()
-in_order(node1)
-print()
-pre_order(node1)
+if __name__ == "__main__":
+    numbers = [10, 15, 8, 3, 9]
+    root = None
+
+    node = TreeNode()
+    node.data = numbers[0]
+    root = node
+
+    # 2번째 원소 부터 마지막 원소까지
+    for number in numbers[1:]:
+        node = TreeNode()
+        node.data = number
+        current = root
+        while True:
+            if number < current.data:
+                if current.left is None:
+                    current.left = node
+                    break
+                current = current.left  # move
+            else:
+                if current.right is None:
+                    current.right = node
+                    break
+                current = current.right  # move
+
+    print("BST 구성 완료")
+
+    post_order(root)
