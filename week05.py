bf1a69f3e0f422f54a4f7a6534fe1450283a16a3
jiyun7765 <jiyun2422@gmail.com>
2025-04-28 15:25:16 +0900

    v2.4 stack

--- week05.py
+++ week05.py
@@ -1,13 +1,16 @@
-s1 = list()
-print(len(s1))
-s1.append("Data structure")  # push
-s1.append("Database")  # push
-print(len(s1))  # size
-print(s1[-1])  # peek
-print(s1)
-print(s1.pop())
-print(s1)
-print(s1.pop())
-print(s1)
-print(s1.pop())
-print(s1)
+def is_vaild_brackets(expression : str) -> bool:
+    stack = []
+    brackeets = {')':'(', '}':'{', ']':'['}
+
+    for letter in expression:
+        if letter in brackeets.values():
+            stack.append(letter)
+        if letter in brackeets.keys():
+            if not stack or stack.pop() != brackeets[letter]:
+                return False
+    return not stack
+
+print(is_vaild_brackets("([2+3])"))
+print(is_vaild_brackets("(2+{3*9}"))
+print(is_vaild_brackets("(2+(3*9)"))
+print(is_vaild_brackets(")2+(3*9)("))
