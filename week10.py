c39c0a9729ad6639df421f0994417177396e2e4e
jiyun7765 <jiyun2422@gmail.com>
2025-05-12 15:12:15 +0900

    v4.0 tree

--- week10.py
+++ week10.py
@@ -79,7 +79,12 @@ def delete(node, value):
             return node.right
         elif node.right is None:
             return node.left
-
+        # 자식 노드가 2개인 경우
+        min_larger_node = node.right
+        while min_larger_node.left:
+            min_larger_node = min_larger_node.left  # move
+        node.data = min_larger_node.data
+        node.right = delete(node.right, min_larger_node.data)
     return node
 
 
